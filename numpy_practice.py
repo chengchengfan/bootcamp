06c31d982669bcc293ac9c9d91e6292ddbe6d566
Chengcheng Fan <cfan@caltech.edu>
2016-09-13 14:28:55 -0700

    Adding in class exercises.

--- numpy_practice.py
+++ numpy_practice.py
@@ -13,3 +13,32 @@ def xa_to_diameter(xa):
     diameter = np.sqrt(xa * 4 / np.pi)
 
     return diameter
+
+def easy_reshape(obj, ncols, order='C'):
+    """
+    Guarentee that reshapping works by defining only one shape parameter.
+    """
+
+    # Reshape
+    arr = np.reshape(obj, (ncols, len(obj)/ncols), order)
+
+    return arr
+
+
+# A = np.array([[6.7, 1.3, 0.6, 0.7],
+#              [0.1, 5.5, 0.4, 2.4],
+#              [1.1, 0.8, 4.5, 1.7],
+#              [0.0, 1.5, 3.4, 7.5]])
+
+# b = np.array([1.1, 2.3, 3.3, 3.9])
+
+
+# Print row 1 of A: A[1,:]
+
+# Print columns 1 and 3 of A: A[:,[1,3]]
+
+# Print the values of every entry in A that are greater than 2: A[A > 2]
+
+# Print the values of diagnal of array np.diag(A)
+
+# A x = b
---
+++ plotting_lines.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy.special
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Generate an array of x values
+x = np.linspace(-15, 15, 400)
+
+# Compute the normalized intensity
+norm_I = 4 * (scipy.special.j1(x) / x)**2
+
+# Plot our computation
+plt.close()
+plt.plot(x, norm_I, marker='.', linestyle='none') # by default, plt.plot plots lines
+plt.margins(0.02)
+plt.xlabel('$x$')
+plt.ylabel('$I(x) / I_0$')
+
+
+# For processing the spike data
+data = np.loadtxt('data/retina_spikes.csv', skiprows=2, delimiter=',')
+t = data[:,0]
+V = data[:,1]
+
+# Close all other plots just in case
+plt.close()
+plt.plot(t,V)
+plt.xlabel('t(ms)')
+plt.ylabel('V(µV)')
+plt.xlim(1395, 1400)
