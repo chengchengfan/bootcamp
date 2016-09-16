1a85a44a534f22722723d6af01ad171e14c432b7
Chengcheng Fan <cfan@caltech.edu>
2016-09-15 22:54:37 -0700

    In class practices.

---
+++ tests/test_eq_calculator.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+import eq_calculator
+
+def test_eq_conc():
+
+    # cab = 0.5 * (ca0 + cb0 + Kd - np.sqrt((ca0 + cb0 + Kd) **2 - 4 * ca0 * cb0))
+    # ca = ca0 - cab
+    # cb = cb0 - cab
+    #
+    # assert Kd == ca * cb / cab
+    # assert ca0 == ca + cab
+    # assert cb0 == cb + cab
+
+    
+
+
+
+    pytest.raises(RuntimeError, "eq_calculator failed")
+
+    return none
